6e11af1e29bce01b9d2ffb9328340790477f09b3
UtkarshaGN <utkarsha.gajanan@gmail.com>
2024-02-09 15:43:39 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Pages from './components/Pages';
 
 function App() {
+  const [data,setData] = useState([]);
+
+  //getting posts from url
+  useEffect(() =>{
+  const paginationFun = async() =>{
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data =  await res.json()
+  setData(data)
+}
+  paginationFun();
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {
+        data.length > 0 ? <Pages data = {data}/> : <p>Loading ....</p>
+      }
+    </>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/hooks/pagination.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+
+ export const usePagination = (perPageRecords, totalPageRecords) => {
+    const totalpages = Math.ceil(totalPageRecords/perPageRecords)
+
+    const[startPageIndex,setStartPageIndex] = useState(0);
+    const[endPageIndex,setEndPageIndex] = useState(perPageRecords-1);
+    const [currentPageIndex, setCurrentPageIndex] = useState(1);
+
+    // function 
+const displayPage = (pageNo) =>{
+    setCurrentPageIndex(pageNo);
+
+    let end_page_index = perPageRecords * pageNo -1;
+    let start_page_index = perPageRecords * pageNo - perPageRecords;
+    setStartPageIndex(start_page_index);
+
+    if(end_page_index >totalPageRecords ){
+        setEndPageIndex(totalPageRecords-1);
+    }else{
+        setEndPageIndex(end_page_index);
+    };
+}
+    return[
+ totalpages,
+ startPageIndex,
+ endPageIndex,
+ currentPageIndex,
+ displayPage
+]
+
+}
+
+
+
